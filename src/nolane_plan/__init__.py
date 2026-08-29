99d7d4ceda19ea8bdfaae3a879a4534398b24d40
Nolane-x <casioreview20@gmail.com>
2026-08-29 21:43:50 +0700

    feat: install crash-safe PlanKernel resume extension

--- src/nolane_plan/__init__.py
+++ src/nolane_plan/__init__.py
@@ -2,7 +2,10 @@
 
 from .kernel import PlanKernel
 from .mission import MissionContract, MissionLedger
+from .resume import install_runtime_extensions
 from .types import RiskClass
+
+install_runtime_extensions(PlanKernel)
 
 __all__ = ["PlanKernel", "MissionContract", "MissionLedger", "RiskClass"]
 __version__ = "0.1.0a1"
