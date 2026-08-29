06508d7702dcdf3a9d08b4bf16aa7eb677175ae1
Nolane-x <casioreview20@gmail.com>
2026-08-29 22:25:03 +0700

    feat: install Wave 3 trust runtime extensions

--- src/nolane_plan/__init__.py
+++ src/nolane_plan/__init__.py
@@ -3,9 +3,11 @@
 from .kernel import PlanKernel
 from .mission import MissionContract, MissionLedger
 from .resume import install_runtime_extensions
+from .trust_runtime import install_trust_runtime
 from .types import RiskClass
 
 install_runtime_extensions(PlanKernel)
+install_trust_runtime(PlanKernel)
 
 __all__ = ["PlanKernel", "MissionContract", "MissionLedger", "RiskClass"]
 __version__ = "0.2.0a1"
